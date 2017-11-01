42a6de3fe8d7c2cabc44fd95322211c52227b1b9
ozxmin <ozxmin@me.com>
2017-11-01 00:48:56 -0700

    corrected port bind

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const path = require('path');
 const express = require('express');
 //joins partial paths to avoid getting /websockets/server/../public
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 
 let app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('server is up');
+app.listen(port, () => {
+    console.log(`server is up at ${port}` );
 });
